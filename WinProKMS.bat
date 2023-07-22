cd06de01925c8da1fcc66c907dc1b93977e67784
MJ1532 <108927987+MJ1532@users.noreply.github.com>
2023-07-22 16:58:50 +0700

    Add files via upload

---
+++ WinProKMS.bat
@@ -0,0 +1,9 @@
+@echo off
+title Windows 10/11 Activator by MJ1532
+slmgr /upk
+slmgr /cpky
+slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+slmgr /skms kms8.msguides.com
+slmgr /ato
+
+pause
---
+++ WinProNormal.bat
@@ -0,0 +1,8 @@
+@echo off
+title Windows 10/11 Activator by MJ1532
+slmgr /upk
+slmgr /cpky
+slmgr /ipk VK7JG-NPHTM-C97JM-9MPGT-3V66T
+slmgr /ato
+
+pause
